5b658a9bafca47eeed23c852f574b6d6fa0fb3a0
Mateo <dzmateozd@gmail.com>
2024-04-05 11:29:31 -0300

    TP3

--- in_string.py
+++ in_string.py
@@ -1,5 +1,11 @@
 def check_vowels():
     # Código a implementar utilizando input.
+    entrada = input(">").lower()
+
+    vocales = ["a", "e", "i", "o", "u"]
+
+    for v in vocales:
+        print(f"Contiene {v}: {v in entrada}")
 
 
 # Para verificar este ejercicio ejecutar el comando
--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,8 @@
 def slice_advanced():
     # Código a implementar utilizando input.
+    entrada = input("> ")
+    # Hello, World!
+    print(entrada[4::2])
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
